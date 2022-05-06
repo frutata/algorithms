e00bbb3dbe3f810cd5afd3d8d6b84889b39dd75f
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-06 11:27:15 -0400

    updated quicksort

--- mern/quicksort_w1_d5.js
+++ mern/quicksort_w1_d5.js
@@ -16,15 +16,15 @@ function partition(nums = [], left = 0, right = nums.length - 1) {
     return count;
 }
 
-function quicksort(nums=[], left=0, right = nums.length-1){
+function quicksort(nums = [], left = 0, right = nums.length - 1) {
     //figure out how you would recursively use partition and quicksort
     //if the start is lesss than the end , then partition the array, and store the index number that we get back in a variable
-    if(left < right){
+    if (left < right) {
         //recursively call quick sort for the left side and also for the right side
-        let index= partition(nums, left, right);
-        let leftside= quicksort(nums, left, index-1 );
-        let rightside= quicksort(nums, index+1, right );
-    } 
+        let index = partition(nums, left, right);
+        let leftside = quicksort(nums, left, index - 1);
+        let rightside = quicksort(nums, index + 1, right);
+    }
     //if the start is not less than the end, return our array
     return nums;
 }
@@ -34,19 +34,30 @@ console.log(quicksort([4, -3, -9, 5, 12, -13, 21, 17, 27]))
   // partition([4, -3, -9, 5, 12, -13, 21, 17, 27]);
 
 /*
-original = [4, -3, -9, 5, 12, -13, 21, 17, 27]
+1st quicksort call-> quicksort([9, -3, -9, 5, 12, 10, 8])
+nums= [9, -3, -9, 5, 12, 10, 8]
+       l                     r
+l = 0;
+r = 6
+partition([9, -3, -9, 5, 12, 10, 8], 0, 6 )
+partitioned array-> [8, -3, -9, 5, 9, 10, 12]
+                                   i
+index = 4
 
-[-13, -3, -9, 4, 12, 5, 21, 17, 27]
-.             X
-[-13, -3, -9, 4, 12, 5, 21, 17, 27]
-    X         x
+leftside quicksort call-> quicksort([8, -3, -9, 5, 9, 10, 12], 0, 3)
+    nums= [8, -3, -9, 5, 9, 10, 12]
+    l = 0
+    r = 3
+    partition([8, -3, -9, 5, 9, 10, 12], 0,3)
+    partitioned array-> [5, -3, -9, 8, 9, 10, 12]
+                                    i
+    index = 3
+    leftside quicksort call-> quicksort([5, -3, -9, 8, 9, 10, 12],0,2)
 
-[-13, -9, -3, 4, 12, 5, 21, 17, 27]
-    X           x
 
-[-13, -9, -3, 4, 5, 12, 21, 17, 27]
-                    x
+    rightside quicksort call-> quicksort([5, -3, -9, 8, 9, 10, 12, 4, 3 )
 
-partition(4,nums.length-1)
+
+righttside quicksort call-> quicksort([8, -3, -9, 5, 9, 10, 12], 5, 6)
 
 */
