8a49148c8b617a773728e6339dec9394d346279f
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-11 11:02:39 -0400

    updated water levels algo

--- mern/w2_d3_water_levels.js
+++ mern/w2_d3_water_levels.js
@@ -10,23 +10,25 @@
 const riverLevels1 = [15, 17, 30];
  const expected1 = 15; // 30 - 15 = 15
 
- 
- const riverLevels3 = [21, 18, 10, 11, 14, 9, 5, 13, 15, 7, 1, 6, 12, 4];
+const riverLevels2 = [15, 17, 30, 14, 5, 16, 25, 9, 3];
+ const expected2 = 20; // 25 - 5 = 20
+
+const riverLevels3 = [21, 18, 10, 11, 14, 9, 5, 13, 15, 7, 1, 6, 12, 4];
  const expected3 = 11; // 12 - 1 = 11
- 
- const riverLevels4 = [1, 5];
- const expected4 = 4;
- 
- const riverLevels5 = [5, 1];
- const expected5 = -1;
- 
- const riverLevels6 = [9, 7, 7, 7];
- const expected6 = -1;
- 
- const riverLevels7 = [42];
- const expected7 = -1;
- 
- /**
+
+const riverLevels4 = [1, 5];
+const expected4 = 4;
+
+const riverLevels5 = [5, 1];
+const expected5 = -1;
+
+const riverLevels6 = [9, 7, 7, 7];
+const expected6 = -1;
+
+const riverLevels7 = [42];
+const expected7 = -1;
+
+/**
   * It ain't much, but it's honest work. A worker who measures water level
   * fluctuations in a river is asked to find the largest fluctuation in water
   * levels during a day, but only for rises in water levels.
@@ -35,9 +37,6 @@ const riverLevels1 = [15, 17, 30];
   * @param {Array<number>} waterLevels Non-empty .
   * @returns {number} The max water-level rise amount or -1 if none.
   */
-
-const riverLevels2 = [15, 17, 30, 14, 5, 16, 25, 9, 3];
-  const expected2 = 20; // 25 - 5 = 20
 
 function measureWaterLevels(waterLevels) {
     let min = waterLevels[0];
